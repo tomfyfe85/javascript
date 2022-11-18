755c8609f4d173d84e1b1c40b9296a894a268e98
Tom <tomfyfe85@gmail.com>
2022-11-18 18:54:07 +0000

    phase2 - part1

---
+++ phase2/my-project/part1/par1Chall/tests/fizzbuzz.test.js
@@ -0,0 +1,32 @@
+ const fizzBuzz = require('../fizzbuzz')
+
+describe('fizzBuzz',() => {
+  it('fizzBuzz if no is divisible by 3 & 5', 
+  () => {
+    expect(fizzBuzz(15)).toBe('fizzbuzz')
+  });
+  
+  it('fizz if no is divisible by 3', 
+  () => {
+    expect(fizzBuzz(3)).toBe('fizz');
+  });
+ 
+  it('buzz if no is divisible by 5',
+  () => {
+    expect(fizzBuzz(5)).toBe('buzz')
+  });
+
+  it('8 if given number is 8',
+  () => {
+    expect(fizzBuzz(8)).toBe(8)
+  });
+
+  it('fizz if no is 18',
+  () => {
+    expect(fizzBuzz(18)).toBe('fizz')
+  });
+
+  it('buzz if no is 20', () => {
+    expect(fizzBuzz(20)).toBe('buzz');
+  });
+});
---
+++ phase2/my-project/part1/par1Chall/tests/scratch.md
@@ -0,0 +1,17 @@
+> fizzBuzz(3); 
+'Fizz'
+
+> fizzBuzz(5);
+'Buzz'
+
+> fizzBuzz(8);
+8
+
+> fizzBuzz(15);
+'FizzBuzz'
+
+> fizzBuzz(18);
+'Fizz'
+
+> fizzBuzz(20);
+'Buzz'
